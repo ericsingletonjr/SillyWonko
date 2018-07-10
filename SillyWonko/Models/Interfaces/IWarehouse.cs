84f4fff8ac16e84a28e941d7b7887e5b1b41d372
Eric Singleton <eric.singleton.jr@gmail.com>
2018-07-10 10:34:26 -0700

    Added interface for data layer called IWarehouse

---
+++ SillyWonko/Models/Interfaces/IWarehouse.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SillyWonko.Models.Interfaces
+{
+    interface IWarehouse
+    {
+        Task<IActionResult> CreateProduct(Product product);
+        Task<IActionResult> GetProductByID(int id);
+        Task<IActionResult> GetProducts();
+        Task<IActionResult> UpdateProduct(int id, Product product);
+        Task<IActionResult> DeleteProduct(int id);
+    }
+}
