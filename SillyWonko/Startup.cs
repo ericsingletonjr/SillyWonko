abde2613d6c5415215c1ecf387958820b31f8e66
Eric Singleton <eric.singleton.jr@gmail.com>
2018-07-10 09:12:07 -0700

    Added Models,Views,Controllers and Data folders to project as well as added mvc and default routes in the startup.cs

--- SillyWonko/Startup.cs
+++ SillyWonko/Startup.cs
@@ -16,6 +16,7 @@ namespace SillyWonko
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +26,8 @@ namespace SillyWonko
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
