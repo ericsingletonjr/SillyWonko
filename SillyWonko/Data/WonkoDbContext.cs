44403fad0b290d3ac47903a7d1c0ce5ff003e895
Jermaine Walker <jermainemwalker@gmail.com>
2018-07-10 10:28:15 -0700

    Setup and initial migration.

--- SillyWonko/Data/WonkoDbContext.cs
+++ SillyWonko/Data/WonkoDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SillyWonko.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- SillyWonko/Startup.cs
+++ SillyWonko/Startup.cs
@@ -5,18 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SillyWonko.Data;
 
 namespace SillyWonko
 {
 	// First commit. Second commit.
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+			builder.AddUserSecrets<Startup>();
+			// For production.
+			Configuration = builder.Build();
+		}
+		public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+			services.AddDbContext<WonkoDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
