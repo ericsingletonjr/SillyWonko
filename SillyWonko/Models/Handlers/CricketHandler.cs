93842b2615229611f96e0995713f21b1139f013b
Eric Singleton <eric.singleton.jr@gmail.com>
2018-07-14 11:54:58 -0700

    Add claim-based property where user cricket status determines specific parts of the site

---
+++ SillyWonko/Models/Handlers/CricketHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SillyWonko.Models.Handlers
+{
+    public class CricketHandler : AuthorizationHandler<CricketRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CricketRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == "BuzzyFizz"))
+            {
+                return Task.CompletedTask;
+            }
+
+            string cricket = context.User.FindFirst(c => c.Type == "BuzzyFizz").Value;
+            
+            if(cricket == requirement.CricketColor)
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SillyWonko/Models/Handlers/CricketRequirement.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SillyWonko.Models.Handlers
+{
+    public class CricketRequirement : IAuthorizationRequirement
+    {
+        public string CricketColor { get; set; }
+
+        public CricketRequirement(string cricketColor)
+        {
+            CricketColor = cricketColor;
+        }
+    }
+}
