78e948379b6bda7d1da6e6cc3fbdaafc9e4cfd4f
Jermaine Walker <jermainemwalker@gmail.com>
2018-07-14 13:17:11 -0700

    Switched home page to landing.

---
+++ SillyWonko/Controllers/ShopController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SillyWonko.Models.Interfaces;
+using SillyWonko.Models.ViewModels;
+
+namespace SillyWonko.Controllers
+{
+    public class ShopController : Controller
+    {
+        private IWarehouse _context;
+
+        public ShopController(IWarehouse context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Action to grab the index of the home controller
+        /// </summary>
+        /// <returns>View</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
