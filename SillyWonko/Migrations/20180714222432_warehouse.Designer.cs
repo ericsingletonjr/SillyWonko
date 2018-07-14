bb74e6c4603c31c95c4a6184ef53329fe6ef4739
Eric Singleton <eric.singleton.jr@gmail.com>
2018-07-14 15:30:04 -0700

    Updated production databas

--- SillyWonko/Migrations/20180714222432_warehouse.Designer.cs
+++ SillyWonko/Migrations/20180714222432_warehouse.Designer.cs
@@ -9,7 +9,7 @@ using SillyWonko.Data;
 namespace SillyWonko.Migrations
 {
     [DbContext(typeof(WonkoDbContext))]
-    [Migration("20180714220032_warehouse")]
+    [Migration("20180714222432_warehouse")]
     partial class warehouse
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -43,16 +43,16 @@ namespace SillyWonko.Migrations
                     b.ToTable("Products");
 
                     b.HasData(
-                        new { ID = 1, Description = "description", Image = "image/candy_one.jpg", Name = "product1", Price = 9.99m, Sku = "ABCD0001" },
-                        new { ID = 2, Description = "description", Image = "image/candy_two.jpg", Name = "product2", Price = 9.99m, Sku = "ABCD0001" },
-                        new { ID = 3, Description = "description", Image = "image/candy_three.jpg", Name = "product3", Price = 9.99m, Sku = "ABCD0001" },
-                        new { ID = 4, Description = "description", Image = "image/candy_four.jpg", Name = "product4", Price = 9.99m, Sku = "ABCD0001" },
-                        new { ID = 5, Description = "description", Image = "image/candy_five.jpg", Name = "product5", Price = 9.99m, Sku = "ABCD0001" },
-                        new { ID = 6, Description = "description", Image = "image/candy_six.jpg", Name = "product6", Price = 9.99m, Sku = "ABCD0001" },
-                        new { ID = 7, Description = "description", Image = "image/candy_sevn.jpg", Name = "product7", Price = 9.99m, Sku = "ABCD0001" },
-                        new { ID = 8, Description = "description", Image = "image/candy_eight.jpg", Name = "product8", Price = 9.99m, Sku = "ABCD0001" },
-                        new { ID = 9, Description = "description", Image = "image/candy_nine.jpg", Name = "product9", Price = 9.99m, Sku = "ABCD0001" },
-                        new { ID = 10, Description = "description", Image = "image/candy_ten.jpg", Name = "product10", Price = 9.99m, Sku = "ABCD0001" }
+                        new { ID = 1, Description = "description", Image = "images/candy_one.jpg", Name = "product1", Price = 9.99m, Sku = "ABCD0001" },
+                        new { ID = 2, Description = "description", Image = "images/candy_two.jpg", Name = "product2", Price = 9.99m, Sku = "ABCD0001" },
+                        new { ID = 3, Description = "description", Image = "images/candy_three.jpg", Name = "product3", Price = 9.99m, Sku = "ABCD0001" },
+                        new { ID = 4, Description = "description", Image = "images/candy_four.jpg", Name = "product4", Price = 9.99m, Sku = "ABCD0001" },
+                        new { ID = 5, Description = "description", Image = "images/candy_five.jpg", Name = "product5", Price = 9.99m, Sku = "ABCD0001" },
+                        new { ID = 6, Description = "description", Image = "images/candy_six.jpg", Name = "product6", Price = 9.99m, Sku = "ABCD0001" },
+                        new { ID = 7, Description = "description", Image = "images/candy_seven.jpg", Name = "product7", Price = 9.99m, Sku = "ABCD0001" },
+                        new { ID = 8, Description = "description", Image = "images/candy_eight.jpg", Name = "product8", Price = 9.99m, Sku = "ABCD0001" },
+                        new { ID = 9, Description = "description", Image = "images/candy_nine.jpg", Name = "product9", Price = 9.99m, Sku = "ABCD0001" },
+                        new { ID = 10, Description = "description", Image = "images/candy_ten.jpg", Name = "product10", Price = 9.99m, Sku = "ABCD0001" }
                     );
                 });
 #pragma warning restore 612, 618
--- SillyWonko/Migrations/20180714222432_warehouse.cs
+++ SillyWonko/Migrations/20180714222432_warehouse.cs
@@ -29,16 +29,16 @@ namespace SillyWonko.Migrations
                 columns: new[] { "ID", "Description", "Image", "Name", "Price", "Sku" },
                 values: new object[,]
                 {
-                    { 1, "description", "image/candy_one.jpg", "product1", 9.99m, "ABCD0001" },
-                    { 2, "description", "image/candy_two.jpg", "product2", 9.99m, "ABCD0001" },
-                    { 3, "description", "image/candy_three.jpg", "product3", 9.99m, "ABCD0001" },
-                    { 4, "description", "image/candy_four.jpg", "product4", 9.99m, "ABCD0001" },
-                    { 5, "description", "image/candy_five.jpg", "product5", 9.99m, "ABCD0001" },
-                    { 6, "description", "image/candy_six.jpg", "product6", 9.99m, "ABCD0001" },
-                    { 7, "description", "image/candy_sevn.jpg", "product7", 9.99m, "ABCD0001" },
-                    { 8, "description", "image/candy_eight.jpg", "product8", 9.99m, "ABCD0001" },
-                    { 9, "description", "image/candy_nine.jpg", "product9", 9.99m, "ABCD0001" },
-                    { 10, "description", "image/candy_ten.jpg", "product10", 9.99m, "ABCD0001" }
+                    { 1, "description", "images/candy_one.jpg", "product1", 9.99m, "ABCD0001" },
+                    { 2, "description", "images/candy_two.jpg", "product2", 9.99m, "ABCD0001" },
+                    { 3, "description", "images/candy_three.jpg", "product3", 9.99m, "ABCD0001" },
+                    { 4, "description", "images/candy_four.jpg", "product4", 9.99m, "ABCD0001" },
+                    { 5, "description", "images/candy_five.jpg", "product5", 9.99m, "ABCD0001" },
+                    { 6, "description", "images/candy_six.jpg", "product6", 9.99m, "ABCD0001" },
+                    { 7, "description", "images/candy_seven.jpg", "product7", 9.99m, "ABCD0001" },
+                    { 8, "description", "images/candy_eight.jpg", "product8", 9.99m, "ABCD0001" },
+                    { 9, "description", "images/candy_nine.jpg", "product9", 9.99m, "ABCD0001" },
+                    { 10, "description", "images/candy_ten.jpg", "product10", 9.99m, "ABCD0001" }
                 });
         }
 
