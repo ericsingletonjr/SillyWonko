9f7a0742e866311e52760eb80195d22b4ed339c4
Jermaine Walker <jermainemwalker@gmail.com>
2018-07-17 11:18:38 -0700

    Finished ICartService interface, and built CartService model.

---
+++ SillyWonko/Models/CartService.cs
@@ -0,0 +1,57 @@
+﻿using SillyWonko.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SillyWonko.Models
+{
+	public class CartService : ICartService
+	{
+		public Task<HttpStatusCode> CreateCart(Cart cart)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<HttpStatusCode> CreateCartItem(CartItem cartItem)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<HttpStatusCode> DeleteCart(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<HttpStatusCode> DeleteCartItem(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<Cart>> GetAllCarts()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Cart> GetCart(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<CartItem>> GetCartItems()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Cart> UpdateCart(int id, Cart cart)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<CartItem> UpdateCartItem(int id, CartItem cartItem)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ SillyWonko/Models/Interfaces/ICartService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SillyWonko.Models.Interfaces
+{
+    interface ICartService
+    {
+		Task<HttpStatusCode> CreateCart(Cart cart);
+		Task<HttpStatusCode> CreateCartItem(CartItem cartItem);
+		Task<List<CartItem>> GetCartItems();
+		Task<HttpStatusCode> DeleteCartItem(int id);
+		Task<HttpStatusCode> DeleteCart(int id);
+		Task<CartItem> UpdateCartItem(int id, CartItem cartItem);
+		Task<Cart> UpdateCart(int id, Cart cart);
+		Task<List<Cart>> GetAllCarts();
+		Task<Cart> GetCart(int id);
+    }
+}
