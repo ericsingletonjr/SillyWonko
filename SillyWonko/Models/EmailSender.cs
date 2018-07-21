57b1573870c5a1e9cc982751435495da760edf4b
Jermaine Walker <jermainemwalker@gmail.com>
2018-07-21 16:55:04 -0700

    Started building class EmailSender.

---
+++ SillyWonko/Models/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SillyWonko.Models
+{
+	public class EmailSender : IEmailSender
+	{
+		IConfiguration Configuration { get; }
+
+		public EmailSender(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+		{
+			var client = new SendGridClient(Configuration["Api_Key"]);
+			var msg = new SendGridMessage();
+
+			msg.SetFrom("admin@wonko.com", "Silly Wonko");
+			msg.AddTo(email);
+			msg.SetSubject(subject);
+			msg.AddContent(MimeType.Html, htmlMessage);
+
+			var response = await client.SendEmailAsync(msg);
+		}
+	}
+}
