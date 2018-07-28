2cc49c70c5bc303efcfae9a4ec0d955d1746c727
Eric Singleton <eric.singleton.jr@gmail.com>
2018-07-28 13:53:03 -0700

    Refactored orders table

--- SillyWonko/Models/Order.cs
+++ SillyWonko/Models/Order.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,5 +15,8 @@ namespace SillyWonko.Models
         public bool IsCheckedOut { get; set; } = false;
         [Display(Name = "Total Price")]
         public decimal TotalPrice { get; set; }
+        public int TotalItems { get; set; }
+        [Column(TypeName="date")]
+        public DateTime OrderDate { get; set; }
     }
 }
